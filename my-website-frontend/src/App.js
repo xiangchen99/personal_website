37e23b4bfb73798f58bbf459aca7af5b54c413f9
xiangchen99 <120740760+xiangchen99@users.noreply.github.com>
2024-03-23 22:36:53 -0400

    continue

--- my-website-frontend/src/App.js
+++ my-website-frontend/src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import AboutMe from './components/AboutMe';
-import Projects from './components/Projects';
-import ContactMe from './components/ContactMe';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import ContactMe from "./components/ContactMe";
 
 function App() {
   return (
     <div className="App">
       <div>
-      <Navbar />
-      <AboutMe />
-      <Projects />
-      <ContactMe />
-    </div>
+        <Navbar />
+        <AboutMe />
+        <Projects />
+        <ContactMe />
+      </div>
     </div>
   );
 }
