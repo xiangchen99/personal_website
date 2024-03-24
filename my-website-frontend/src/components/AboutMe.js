b34b674a5af4930c1883f6ad3dc696f410e29fac
xiangchen99 <120740760+xiangchen99@users.noreply.github.com>
2024-03-23 22:33:11 -0400

    continue working on it

--- my-website-frontend/src/components/AboutMe.js
+++ my-website-frontend/src/components/AboutMe.js
@@ -7,12 +7,13 @@ function AboutMe() {
       <h2>About Me</h2>
       <div className="about-content">
         <img
-          src="your-profile-picture.jpg"
+          src={process.env.PUBLIC_URL + "/logo192.png"}
           alt="Your Profile"
           className="profile-pic"
         />
         <p>
-          I'm a passionate web developer with a love for hiking!
+          Hello my name is Xiang! I am a software developer with a passion for machine learning and big data, and I am currently looking for an internship in software engineering.
+          I have experience in Data Science, Machine Learning, and Full Stack Web Development. I am proficient in Python, Java, JavaScript, and SQL. I have experience with libraries such as TensorFlow, Keras, Pandas, NumPy, and Scikit-learn. I have also worked with frameworks such as React, Node.js, and Express.js. I am a quick learner and I am always looking to expand my skill set.
         </p>
       </div>
     </section>
